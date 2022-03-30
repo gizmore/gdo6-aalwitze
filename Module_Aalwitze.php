ac0a58b8b09568cdf316cde748e1d05465ad91ce
gizmore <gizmore@wechall.net>
2022-03-30 07:27:21 +0200

    v6.11.4-r6331_favicon

--- Module_Aalwitze.php
+++ Module_Aalwitze.php
@@ -3,9 +3,11 @@ namespace GDO\Aalwitze;
 
 use GDO\Core\GDO_Module;
 use GDO\Language\Module_Language;
+use GDO\Website\Module_Website;
 
 /**
  * Cheap copy of mettwitze with some text changes.
+ * 
  * @author gizmore
  * @since 6.11.4
  */
@@ -16,18 +18,19 @@ final class Module_Aalwitze extends GDO_Module
 	##############
 	public $module_priority = 91;
 	
-	public function getTheme() { return 'mettwitze'; } # own theme for tpl overrides @see thm folder.
 	public function onLoadLanguage() { return $this->loadLanguage('lang/aalwitze'); }
-	public function getDependencies() { return ['Mettwitze']; }
-	public function getClasses()
+
+	public function getDependencies()
 	{
-		# Entity tables
 		return [
+			'Mettwitze',
+			'Favicon', 'Website', # new stuff :)
 		];
 	}
 	
 	public function onInstall()
 	{
+		Module_Website::instance()->saveConfigVar('theme_color', '#72B7BF');
 		Module_Language::instance()->saveConfigVar('languages', '["de","en"]');
 	}
 	
